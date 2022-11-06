209c73a60ee412e4fece91a1f69d363611d6391a
Marcin Staszczyk <staszczyk.marcin@gmail.com>
2022-11-06 16:33:07 +0100

    First part of documentation

--- src/app/util/rxjs/selector/Selector.ts
+++ src/app/util/rxjs/selector/Selector.ts
@@ -16,10 +16,45 @@ import { environment } from '../../../../environments/environment';
 import { callProgress, DeferredCallWithProgress } from '../../progress/callProgress';
 import { Single } from '../Single';
 
+/**
+ * Converts any Observable into Selector observable.
+ *
+ * Convenience function. See Selector.from() for more details.
+ *
+ * If using with RxJS operators map or switchMap, see map() or asyncMap() method instead.
+ * If using with RxJS combineLatest, see combineWith() method instead.
+ *
+ * @example
+ * // Basic usage
+ * select(observable$);
+ *
+ * // Convert to selector after using RxJS operator(s)
+ * select(
+ *   selectedItem$.pipe(
+ *     filter((value) => Boolean(value))
+ *   )
+ * );
+ *
+ * // Preferred declarative usage
+ * class Component {
+ *   readonly selectedItemOrNull$: Selector<Item | null> = ...;
+ *   readonly selectedItem$: Selector<Item> = select(this.selectedItem$.pipe(
+ *     filter((value) => Boolean(value))
+ *   ));
+ * }
+ */
 export function select<T>(source$: Observable<T>): Selector<T> {
     return Selector.from(source$);
 }
 
+/**
+ * Selector - type of observable dedicated to processing of data in a Reactive way.
+ *
+ * Together with declarative programming enforces good practises of Reactive Data Flow:
+ * * Re-usage of streams on multiple subscriptions.
+ * * Not passing same value multiple times down the stream.
+ * * Conversion and combination of streams in a performant manner.
+ */
 export class Selector<T> extends Observable<T> {
 
     /**
@@ -28,6 +63,25 @@ export class Selector<T> extends Observable<T> {
      */
     readonly passingValues$ = new Subject<T>();
 
+    /**
+     * Function which converts Observable into Selector.
+     * Conversion is done by applying best practises of Reactive Data Flow into Observable:
+     * * distinctUntilChanged() RxJS operator - Not passing same value multiple times down the stream.
+     * * share() RxJS operator - Sharing data between multiple subscriptions.
+     *
+     * Provided _source$_ should be observable with reactive data.
+     * Use on non-reactive observables (like Single observable) is not recommended (use ResourceCache instead).
+     * Use on streams of actions/events is not recommended.
+     *
+     * It's possible to configure how selector behaviours will be applied:
+     * * config?.equals: _(previous: T, current: T) => boolean_ - defines function to distinct if stream value actually changed.
+     * By default, equality check is done by object references.
+     * * config?.shareConfig: _ShareConfig<T>_ - defines configuration passed to share RxJS operator.
+     *
+     * @param source$ observable with reactive data.
+     * @param config enables configuration of selector behaviors
+     * @return {Selector} observable
+     */
     static from<T>(
         source$: Observable<T>,
         config?: SourceConfig<T>
