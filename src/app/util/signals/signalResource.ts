b623de0b0d98c8d9446d30168c5d9bed85b198bc
Marcin Staszczyk <staszczyk.marcin@gmail.com>
2023-08-18 11:42:26 +0200

    Extract toAsyncSignal from signalResource

--- src/app/util/signals/signalResource.ts
+++ src/app/util/signals/signalResource.ts
@@ -4,20 +4,18 @@ import { equalArrayReferences } from '@/util/signals/internal/equalArrayReferenc
 import { SafeUnwrapSignals } from '@/util/signals/internal/SafeUnwrapSignals';
 import { splitParams } from '@/util/signals/internal/splitParams';
 import { safeComputed } from '@/util/signals/safeComputed';
+import { FullAsyncSignal, toAsyncSignal } from '@/util/signals/toAsyncSignal';
 import { Tuple } from '@/util/types/Tuple';
-import { computed, DestroyRef, inject, signal, Signal, untracked, WritableSignal } from '@angular/core';
-import { ReplaySubject, Subscription, switchMap } from 'rxjs';
+import { inject, Injector, signal, Signal, untracked, WritableSignal } from '@angular/core';
+import { ReplaySubject, switchMap, tap } from 'rxjs';
 
-export type SignalResource<T> = Exclude<WritableSignal<T>, 'mutate'>
+export type SignalResource<T> = Omit<WritableSignal<T>, 'mutate'>
     & AsyncSignal<T>
     & {
         loading: Signal<boolean>; // call will NOT activate request
-        error: Signal<undefined | unknown>; // call will NOT activate request (TODO confirm)
     };
 
 type SignalResourceOptions<T> = { initialValue?: T };
-
-const NOT_LOADED = Symbol('NOT_LOADED');
 
 export function signalResource<R>(asyncCall: () => Single<R>, options: SignalResourceOptions<R>): SignalResource<R>;
 export function signalResource<R>(asyncCall: () => Single<R>): SignalResource<R>;
@@ -41,78 +39,36 @@ export function signalResource<ST extends Tuple<Signal<any>>, R>(
         { equal: equalArrayReferences }
     );
 
-    const destroyRef: DestroyRef = inject(DestroyRef);
+    const injector: Injector = inject(Injector);
 
-    const writableSignal = signal<R | typeof NOT_LOADED>(NOT_LOADED);
-    const error = signal<unknown | undefined>(undefined);
     const loading = signal(false);
 
     let lastValues: SafeUnwrapSignals<ST> | undefined;
     let sourceValuesSubject$ = new ReplaySubject<SafeUnwrapSignals<ST>>(1);
-    let subscription: Subscription | undefined;
 
-    const baseResult = computed(
-        () => {
-            const values: SafeUnwrapSignals<ST> | undefined = sourceValues();
-            if (!values || values === lastValues) {
-                return writableSignal();
+    const asyncSignal: FullAsyncSignal<R> = toAsyncSignal(
+        sourceValuesSubject$.pipe(
+            switchMap((values: SafeUnwrapSignals<ST>) => {
+                untracked(() => loading.set(true));
+                return call(...values)
+            }),
+            tap(() => untracked(() => loading.set(false)))
+        ),
+        {
+            injector,
+            runInComputedContext: () => {
+                const values: SafeUnwrapSignals<ST> | undefined = sourceValues();
+                if (!values || values === lastValues) {
+                    return;
+                }
+
+                lastValues = values;
+                sourceValuesSubject$.next(values);
             }
-
-            lastValues = values;
-            sourceValuesSubject$.next(values);
-
-            if (!subscription) {
-                subscription = sourceValuesSubject$.pipe(
-                    switchMap((values: SafeUnwrapSignals<ST>) => {
-                        untracked(() => loading.set(true));
-                        return call(...values)
-                    })
-                ).subscribe({
-                    next: (value: R) => untracked(() => {
-                        writableSignal.set(value);
-                        loading.set(false);
-                    }),
-                    error: error => untracked(() => error.set(error))
-                });
-                destroyRef.onDestroy(subscription.unsubscribe.bind(subscription))
-            }
-
-            return writableSignal();
         }
     );
 
-    const result: Signal<R> = computed(() => {
-        const value = baseResult();
-        if (value === NOT_LOADED) {
-            if (options?.initialValue) {
-                return options.initialValue;
-            }
-            throw new Error('Value not yet loaded. Check ready() before getting value, or provide initialValue option.');
-        } else {
-            return value;
-        }
-    })
-
-    const ready: Signal<boolean> = computed(() => {
-        const result = baseResult();
-        return result !== NOT_LOADED;
-    })
-
-    return Object.assign(result, {
-        set: (value: R) => writableSignal.set(value),
-        update: (updateFn: (value: R) => R) => writableSignal.update((value: R | typeof NOT_LOADED) => {
-            if (value === NOT_LOADED) {
-                throw new Error('Value not yet loaded. Check ready() before setting value, or provide initialValue option.');
-            } else {
-                return updateFn(value);
-            }
-        }),
-        mutate: (_mutateFn: (value: R) => void) => {
-            throw new Error('Mutate is not working on SignalResource');
-        },
-        asReadonly: () => result,
-        ready,
-        loading,
-        error,
+    return Object.assign(asyncSignal, {
+        loading
     })
 }
---
+++ src/app/util/signals/toAsyncSignal.ts
@@ -0,0 +1,82 @@
+import { AsyncSignal } from '@/util/signals/AsyncSignal';
+import { assertInInjectionContext, computed, DestroyRef, inject, Signal, signal, untracked, WritableSignal } from '@angular/core';
+import { ToSignalOptions } from '@angular/core/rxjs-interop';
+import { Observable, Subscribable, Unsubscribable } from 'rxjs';
+
+export type FullAsyncSignal<T> =
+    AsyncSignal<T>
+    & Omit<WritableSignal<T>, 'mutate'>
+    & {
+        error: Signal<unknown | undefined>; // call will NOT activate request (TODO confirm)
+    };
+
+const NOT_LOADED = Symbol('NOT_LOADED');
+
+export type ToAsyncSignalOptions<T> = Pick<ToSignalOptions<T>, 'initialValue' | 'injector'> & {
+    runInComputedContext?: () => void
+};
+
+export function toAsyncSignal<T, U = undefined>(
+    source: Observable<T>|Subscribable<T>,
+    options?: Omit<ToAsyncSignalOptions<U>, 'requireSync' | 'manualCleanup'>
+): FullAsyncSignal<T | U> {
+    !options?.injector && assertInInjectionContext(toAsyncSignal);
+    const destroyRef: DestroyRef = options?.injector?.get(DestroyRef) ?? inject(DestroyRef);
+
+    const writableSignal = signal<T | U | typeof NOT_LOADED>(options?.initialValue ?? NOT_LOADED);
+    const error = signal<unknown | undefined>(undefined);
+
+    let subscription: Unsubscribable | undefined;
+
+    const baseResult = computed(
+        () => {
+            options?.runInComputedContext?.()
+
+            if (!subscription) {
+                subscription = source.subscribe({
+                    next: (value: T) => untracked(() => {
+                        writableSignal.set(value);
+                    }),
+                    error: error => untracked(() => error.set(error))
+                });
+                destroyRef.onDestroy(subscription.unsubscribe.bind(subscription))
+            }
+
+            return writableSignal();
+        }
+    );
+
+    const result: Signal<T | U> = computed(() => {
+        const value = baseResult();
+        if (value === NOT_LOADED) {
+            if (options?.initialValue) {
+                return options.initialValue;
+            }
+            throw new Error('Value not yet loaded. Check ready() before getting value, or provide initialValue option.');
+        } else {
+            return value;
+        }
+    })
+
+    const ready: Signal<boolean> = computed(() => {
+        const result = baseResult();
+        return result !== NOT_LOADED;
+    })
+
+    return Object.assign(result, {
+        set: (value: T) => writableSignal.set(value),
+        update: (updateFn: (value: T | U) => T) => writableSignal.update((value: T | U | typeof NOT_LOADED) => {
+            if (value === NOT_LOADED) {
+                throw new Error('Value not yet loaded. Check ready() before setting value, or provide initialValue option.');
+            } else {
+                return updateFn(value);
+            }
+        }),
+        // mutate: (_mutateFn: (value: R) => void) => {
+        //     throw new Error('Mutate is not working on FullAsyncSignal');
+        // },
+        asReadonly: () => result,
+        ready,
+        error,
+    })
+}
