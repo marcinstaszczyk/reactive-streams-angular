2e294c288c20c0c2d07d91213e0cd84b7766b641
Marcin Staszczyk <staszczyk.marcin@gmail.com>
2024-04-21 17:08:26 +0200

    Make routes lazy to see how it affects stats.json

--- src/app/AppRoutingModule.ts
+++ src/app/AppRoutingModule.ts
@@ -1,17 +1,13 @@
-import { BoardContentComponent } from '@/board-content/BoardContentComponent';
-import { CarouselModule } from '@/carousel/CarouselModule';
-import { DiamondModule } from '@/diamond/DiamondModule';
-import { PerformanceComponent } from '@/performance/PerformanceComponent';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-	{ path: 'carousel', loadChildren: () => CarouselModule },
-	{ path: 'diamond', loadChildren: () => DiamondModule },
-    { path: 'board/:boardId', component: BoardContentComponent },
-    { path: 'performance', component: PerformanceComponent },
+	{ path: 'carousel', loadChildren: () => import('@/carousel/CarouselModule').then(m => m.CarouselModule) },
+	{ path: 'diamond', loadChildren: () => import('@/diamond/DiamondModule').then(m => m.DiamondModule) },
+    { path: 'board/:boardId', loadComponent: () => import('@/board-content/BoardContentComponent').then(m => m.BoardContentComponent) },
+    { path: 'performance', loadComponent: () => import('@/performance/PerformanceComponent').then(m => m.PerformanceComponent) },
     { path: '**', redirectTo: '' },
 ];
 
